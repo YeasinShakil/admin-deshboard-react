f0e52be28d559c0dfd527cd88e536470f1cde987
Yeasin_Shakil <shakilrock1998@gmail.com>
2023-03-16 19:51:48 +0600

    top bar and dark and light mood setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css'
+import Topbar from './pages/global/Topbar';
+import { ColorModeContext, useMode } from './theme'
 
 function App() {
-
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-     shakil
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline></CssBaseline>
+        <div className="app">
+          <main className="content">
+
+          <Topbar></Topbar>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
